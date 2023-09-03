9d38f7910213853dc4c0e25ba14e9c5241d3607c
unknown <yenloned@gmail.com>
2023-09-03 17:44:45 +0800

    update

--- comps/About.tsx
+++ comps/About.tsx
@@ -22,7 +22,7 @@ const About = () =>{
                         <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="w-6 h-6" viewBox="0 0 16 16">
                             <path d="M3.05 3.05a7 7 0 0 0 0 9.9.5.5 0 0 1-.707.707 8 8 0 0 1 0-11.314.5.5 0 0 1 .707.707zm2.122 2.122a4 4 0 0 0 0 5.656.5.5 0 1 1-.708.708 5 5 0 0 1 0-7.072.5.5 0 0 1 .708.708zm5.656-.708a.5.5 0 0 1 .708 0 5 5 0 0 1 0 7.072.5.5 0 1 1-.708-.708 4 4 0 0 0 0-5.656.5.5 0 0 1 0-.708zm2.122-2.12a.5.5 0 0 1 .707 0 8 8 0 0 1 0 11.313.5.5 0 0 1-.707-.707 7 7 0 0 0 0-9.9.5.5 0 0 1 0-.707zM10 8a2 2 0 1 1-4 0 2 2 0 0 1 4 0z"/>
                         </svg>
-                        <div className="flex gap-2"><div>Interest: </div><div>Singing, Workout, Tchoukball</div></div>
+                        <div className="flex gap-2"><div>Interest: </div><div>Singing, Music Production, Workout</div></div>
                     </div>
                 </div>
 
